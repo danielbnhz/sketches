512c05302bcca4ab47529cfaee5efbc2606e4bb9
Daniel <danielbnhz@gmail.com>
2025-04-26 23:47:06 -0700

    made a little ball bounce with some interesting triangles in the background

--- sketch.js
+++ sketch.js
@@ -7,24 +7,28 @@ function draw() {
   
   frameCount = frameCount > 500 ? 0 : frameCount
 
-  let movementX = frameCount 
-
+  let movementX = frameCount /2
+  frameRate(20)
 
 
 
   rectMode(CENTER);
   const space = 40;
   const n = 400;
-
+  let seed_num = 2
   for (x = 0; x < n; x+=space){
+    let num = Math.random() * seed_num
+    seed_num > 40 ? 2 : seed_num * 2.3
       for (y = 0; y < n; y+=space){
-        line(x, y , x + space, y);
-        line(x,y,x,y + space)
+        fill(222+num,222,222);
+        triangle(x, y, x + space, y,2,2);
+        fill(10+num,10,10+num)
+        triangle(x,y,x,y + space,2,2)
       }
 
       
   }
-
-  circle(movementX,200, 40)
+  fill(255,255,255)
+  circle(movementX,200 + sin(movementX)*40, 40)
 
 }
