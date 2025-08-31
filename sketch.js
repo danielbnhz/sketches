cc23ed3e6bb03f2d239ba44db81dd40551e4a6c9
Daniel <danielbnhz@gmail.com>
2025-08-31 03:36:45 -0700

    Changed the sketch into a cool particle-field

--- sketch.js
+++ sketch.js
@@ -1,34 +1,57 @@
+let particles = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 150; i++) {
+    particles.push(new Particle());
+  }
 }
 
 function draw() {
-  background(220);
+  background(20, 20, 30, 100); // dark translucent background (trail effect)
   
-  frameCount = frameCount > 500 ? 0 : frameCount
-
-  let movementX = frameCount /2
-  frameRate(20)
-
-
-
-  rectMode(CENTER);
-  const space = 40;
-  const n = 400;
-  let seed_num = 2
-  for (x = 0; x < n; x+=space){
-    let num = Math.random() * seed_num
-    seed_num > 40 ? 2 : seed_num * 2.3
-      for (y = 0; y < n; y+=space){
-        fill(222+num,222,222);
-        triangle(x, y, x + space, y,2,2);
-        fill(10+num,10,10+num)
-        triangle(x,y,x,y + space,2,2)
-      }
-
-      
+  for (let p of particles) {
+    p.update();
+    p.edges();
+    p.show();
   }
-  fill(255,255,255)
-  circle(movementX,200 + sin(movementX)*40, 40)
+}
 
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = p5.Vector.random2D().mult(random(0.5, 2));
+    this.acc = createVector(0, 0);
+    this.size = random(2, 6);
+    this.color = color(random(100, 255), random(100, 255), random(200, 255), 200);
+  }
+
+  update() {
+    this.pos.add(this.vel);
+  }
+
+  edges() {
+    // bounce on edges
+    if (this.pos.x < 0 || this.pos.x > width) this.vel.x *= -1;
+    if (this.pos.y < 0 || this.pos.y > height) this.vel.y *= -1;
+  }
+
+  show() {
+    noStroke();
+    fill(this.color);
+    circle(this.pos.x, this.pos.y, this.size);
+
+    // optional: connect particles that are close
+    for (let other of particles) {
+      let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+      if (d < 100) {
+        stroke(150, 150, 255, 50);
+        line(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+      }
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
